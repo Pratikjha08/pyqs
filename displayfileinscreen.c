cc584129a21df640983c7ffcb8b27bda80a504ae
Pratik <pratikjha4926@gmail.com>
2023-06-08 11:18:45 +0545

    Added a functionality of re prompting user if wrong file name is given.

--- displayfileinscreen.c
+++ displayfileinscreen.c
@@ -1,22 +1,24 @@
-//WAP to display the content of file on screen and prompt user for the source file
+//WAP to display the content of file on screen and prompt user for the source file and repropt the user to enter correct file name if worng is entered
 
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
 
 int main(){
+    int chance = 0;
     char fname[20];
     printf("Enter file name: ");
     scanf("%s",fname);
     FILE *fp;
     fp = fopen(fname,"r");
-    if (fp == NULL){
-        printf("File not found");
-        getch();
-        exit(0);
+    while (fp==NULL && chance<3){
+        printf("Enter correct filename: ");
+        scanf("%s",fname);
+        fp = fopen(fname,"r");
+        chance++;
     }
     char ch;
-    while (!feof(fp)){
+    while (!feof(fp) && !(fp == NULL)){
         ch = getc(fp);
         printf("%c",ch);
     }
